773eb8486faad1b1d07015e3164b61c748beda20
HinanawiTenshi <dr.paper@live.com>
2017-11-16 17:23:09 +0800

    [Fix]Class Character fixed.

--- app/src/main/java/com/threegiants/e_three_kingdom/Character.java
+++ app/src/main/java/com/threegiants/e_three_kingdom/Character.java
@@ -3,64 +3,53 @@ package com.threegiants.e_three_kingdom;
 import android.graphics.Bitmap;
 
 /**
- * Created by applecz on 2017/11/4.
+ * Class Character
+ * Keep down a single character's information.
  */
 
 public class Character {
-    //id、name、icon、gender、birth、hometown、camp
-    //id is unique
-    private String id;
+
+    private int id;
     private String name;
-    private Bitmap icon;
     private String gender;
     private String birth;
-    private String home_town;
+    private String homeTown;
     private String camp;
+    private String shortDescription;
     private String description;
-    public Character(String i, String n, Bitmap ic, String g, String b, String ht, String c, String d) {
-        id=i;
+    private Bitmap icon;
+
+    Character(
+        int i, String n, String g, String b, String ht, String c, String sd, String d, Bitmap ic
+    ) {
+        id = i;
         name = n;
+        gender = g;
+        birth = b;
+        homeTown = ht;
+        camp = c;
+        shortDescription = sd;
+        description = d;
         icon = ic;
-        gender=g;
-        home_town=ht;
-        camp=c;
-        description=d;
     }
-    public String getId() {return id;}
-    public String getName(){
-        return name;
-    }
-    public Bitmap getIcon(){
-        return icon;
-    }
-    public String getGender(){
-        return gender;
-    }
-    public String getBirth(){
-        return birth;
-    }
-    public String getHomeTown(){
-        return home_town;
-    }
-    public String getCamp(){
-        return camp;
-    }
-    public String getDescription() {return description;}
-    public void setName(String n){name=n;}
-    public void setIcon(Bitmap ic){
-        icon=ic;
-    }
-    public void setGender(String g){
-        gender=g;
-    }
-    public void setBirth(String b){
-        birth=b;
-    }
-    public void setHomeTown(String ht){
-        home_town=ht;
-    }
-    public void setCamp(String c){
-        camp=c;
-    }
-    public void setDescription(String d){description=d;}
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getGender() { return gender; }
+    public String getBirth() { return birth; }
+    public String getHomeTown() { return homeTown; }
+    public String getCamp() { return camp; }
+    public String getShortDescription() { return shortDescription; }
+    public String getDescription() { return description; }
+    public Bitmap getIcon() { return icon; }
+
+    public void setId(int i) { id = i; }
+    public void setName(String n){ name = n; }
+    public void setGender(String g) { gender = g; }
+    public void setBirth(String b) { birth = b; }
+    public void setHomeTown(String ht) { homeTown = ht; }
+    public void setCamp(String c) { camp = c; }
+    public void setShortDescription(String sd) { shortDescription = sd; }
+    public void setDescription(String d) { description = d; }
+    public void setIcon(Bitmap ic){ icon = ic; }
 }
